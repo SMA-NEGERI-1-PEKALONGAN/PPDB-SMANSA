ece9017da0b12f3ed11f140eb4cb23e7976dbeeb
lammmzt <alam0098@stmik-wp.ac.id>
2024-05-31 16:40:13 +0700

    add fiture

--- app/Controllers/antrianController.php
+++ app/Controllers/antrianController.php
@@ -6,7 +6,14 @@ use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 use Hermawan\DataTables\DataTable;
 use App\Models\antrianModel;
-
+use Ramsey\Uuid\Uuid;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode;
 
 class antrianController extends BaseController
 {
@@ -37,15 +44,158 @@ class antrianController extends BaseController
                 <div class="dropdown">
 					<a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown"> <i class="dw dw-more"></i></a>
 						<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                            <button class="dropdown-item view_user" id="'.$row->id_antrian.'"><i class="dw dw-eye"></i> View</a>
-                            <button class="dropdown-item edit_user" id="'.$row->id_antrian.'"><i class="dw dw-edit2"></i> Edit</button>
-                            <button class="dropdown-item reset_pass" id="'.$row->id_antrian.'"><i class="dw dw-refresh2"></i> Reset Password</button>
-							<button class="dropdown-item delete_user" id="'.$row->id_antrian.'"><i class="dw dw-delete-3"></i> Delete</button>
+                            <button class="dropdown-item detailsAntrian" id="'.$row->id_antrian.'"><i class="dw dw-eye"></i> View</a>
+                            <button class="dropdown-item edit_antrian" id="'.$row->id_antrian.'"><i class="dw dw-edit2"></i> Edit</button>
+							<button class="dropdown-item delete_antrian" id="'.$row->id_antrian.'"><i class="dw dw-delete-3"></i> Delete</button>
 						</div>
 				</div>
                 ';
             }, 'last')
             ->toJson(true);
+    }
+
+    public function store(){
+         $validation =  \Config\Services::validation();
+          $validation->setRules([
+            'nama_siswa' => [
+                'label' => 'Nama Siswa',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi.',
+                ]
+            ],
+            'nisn' => [
+                'label' => 'NISN',
+                'rules' => 'required|is_unique[antrian.nisn]',
+                'errors' => [
+                    'required' => '{field} harus diisi.',
+                    'is_unique' => '{field} sudah terdaftar.'
+                ]
+            ],
+            'asal_sekolah' => [
+                'label' => 'Asal Sekolah',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi.'
+                ]
+            ],
+            'alamat' => [
+                'label' => 'Alamat',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi.'
+                ]
+            ],
+            'no_tlp' => [
+                'label' => 'No. Telepon',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi.'
+                ]
+            ],
+            'jenis_kelamin' => [
+                'label' => 'Jenis Kelamin',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi.'
+                ]
+            ],
+            'jalur_pendaftaran' => [
+                'label' => 'Jalur Pendaftaran',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi.'
+                ]
+            ],
+            'kode_pendaftaran' => [
+                'label' => 'Kode Pendaftaran',
+                'rules' => 'required|is_unique[antrian.kode_pendaftaran]',
+                'errors' => [
+                    'required' => '{field} harus diisi.',
+                    'is_unique' => '{field} sudah terdaftar.'
+                ]
+            ],
+          ]);
+          
+            if (!$validation->withRequest($this->request)->run()) {
+                return $this->response->setJSON([
+                    'error' => true,
+                    'data' => $validation->getErrors(),
+                    'status' => '422'
+                ]);
+            } else {
+                $uuid = Uuid::uuid4();
+                $id_antrian = $uuid->toString();
+
+                $result = Builder::create()
+                    ->writer(new PngWriter())
+                    ->writerOptions([])
+                    ->data($id_antrian)
+                    ->encoding(new Encoding('UTF-8'))
+                    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+                    ->size(300)
+                    ->margin(10)
+                    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+                    ->logoPath('Assets/LOGO SMANSA.png')
+                    ->logoResizeToWidth(50)
+                    ->logoPunchoutBackground(true)
+                    // ->labelText('This is the label')
+                    // ->labelFont(new NotoSans(20))
+                    // ->labelAlignment(LabelAlignment::Center)
+                    ->validateResult(false)
+                    ->build();
+                    
+                $result->saveToFile('Assets/qr_code/' . $id_antrian . '.png');
+
+                $data = [
+                    'id_antrian' => $id_antrian,
+                    'nama_siswa' => $this->request->getPost('nama_siswa'),
+                    'nisn' => $this->request->getPost('nisn'),
+                    'asal_sekolah' => $this->request->getPost('asal_sekolah'),
+                    'alamat' => $this->request->getPost('alamat'),
+                    'no_tlp' => $this->request->getPost('no_tlp'),
+                    'jenis_kelamin' => $this->request->getPost('jenis_kelamin'),
+                    'jalur_pendaftaran' => $this->request->getPost('jalur_pendaftaran'),
+                    'kode_pendaftaran' => $this->request->getPost('kode_pendaftaran'),
+                    'qr_code' => $id_antrian . '.png',
+                    'status_antrian' => '1',
+                    'tanggal_antrian' => date('Y-m-d H:i:s'),
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ];
+                $this->antrianModel->insert($data);
+                return $this->response->setJSON([
+                    'error' => false,
+                    'data' => 'Data berhasil disimpan',
+                    'status' => '200'
+                ]);
+            }
+    }
+
+    public function edit(){
+        $id = $this->request->getPost('id');
+        $data = $this->antrianModel->getAntrian($id);
+        return $this->response->setJSON([
+            'error' => false,
+            'data' => $data,
+            'status' => '200'
+        ]);
+    }
+
+    public function destroy(){
+        $id = $this->request->getPost('id');
+        // remove qr code
+        $qr_code = $this->antrianModel->select('qr_code')->where('id_antrian', $id)->first();
+        if ($qr_code->qr_code != 'default.png') {
+            unlink('Assets/qr_code/' . $qr_code->qr_code);
+        }
+        // remove data
+        $this->antrianModel->delete($id);
+        return $this->response->setJSON([
+            'error' => false,
+            'data' => 'Data berhasil dihapus',
+            'status' => '200'
+        ]);
     }
 
 }
--- app/Views/Admin/Antrian/index.php
+++ app/Views/Admin/Antrian/index.php
@@ -10,7 +10,7 @@
                         <h4 class="text-blue h4">Data Antrian</h4>
                     </div>
                     <div class="col-sm-6 text-right">
-                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#addUser" type="button">
+                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#addAntrian" type="button">
                             <i class="icon-copy fa fa-plus" aria-hidden="true"></i>
                         </a>
                     </div>
@@ -33,7 +33,8 @@
     </div>
 </div>
 
-<div class="modal fade" id="addUser" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+<div class="modal fade" id="addAntrian" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
@@ -44,7 +45,7 @@
                     ×
                 </button>
             </div>
-            <form id="form_tambah_user">
+            <form id="form_tambah_antrian" enctype="multipart/form-data">
                 <div class="modal-body">
                     <div class="form-group row">
                         <label for="nama_siswa" class="col-sm-4 col-form-label">nama_siswa<span
@@ -52,7 +53,7 @@
                         <div class="col-sm-8">
                             <input type="text" class="form-control required" id="nama_siswa" name="nama_siswa"
                                 placeholder="Masukan nama">
-                            <div class="form-control-feedback " id="errornama"></div>
+                            <div class="form-control-feedback " id="errornama_siswa"></div>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -77,7 +78,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="kode_pendaftaran" class="col-sm-4 col-form-label">Kode Pendaftarab<span
+                        <label for="kode_pendaftaran" class="col-sm-4 col-form-label">Kode Pendaftaran<span
                                 class="rq">*</span></label></label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control required" id="kode_pendaftaran"
@@ -113,18 +114,120 @@
                             <div class="form-control-feedback " id="erroralamat"></div>
                         </div>
                     </div>
-
+                    <div class="form-group row ">
+                        <label for="jalur_pendaftaran" class="col-sm-4 col-form-label">Jalur Pendaftaran<span
+                                class="rq">*</span></label></label>
+                        <div class="col-sm-8">
+                            <select class="form-control required" id="jalur_pendaftaran" name="jalur_pendaftaran">
+                                <option value="">Pilih Jalur Pendaftaran</option>
+                                <option value="Zonasi">Zonasi</option>
+                                <option value="Afirmasi">Afirmasi</option>
+                                <option value="Perpindahan Orang Tua">Perpindahan Orang Tua</option>
+                                <option value="Prestasi">Prestasi</option>
+                            </select>
+                            <div class="form-control-feedback " id="errorjalur_pendaftaran"></div>
+                        </div>
+                    </div>
 
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">
                         Batal
                     </button>
-                    <button type="submit" class="btn btn-primary" id="btn_tambah_user">
+                    <button type="submit" class="btn btn-primary" id="btn_tambah_antiran">
                         Tambah
                     </button>
                 </div>
             </form>
+        </div>
+    </div>
+</div>
+
+<!-- details -->
+<div class="modal fade" id="detailsAntrian" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myLargeModalLabel">
+                    Detail Antrian
+                </h4>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                    ×
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <!-- add image -->
+                <div class="form-group row">
+                    <div class="col-sm-12 text-center">
+                        <img src="" alt="" id="detailqr_code" class="img-thumbnail">
+                    </div>
+                </div>
+                <hr>
+                <div class="form-group row">
+                    <label for="nama_siswa" class="col-sm-4 col-form-label">Nama Siswa</label></label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="detailnama_siswa" name="nama_siswa"
+                            placeholder="Nama" readonly>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="kode_pendaftaran" class="col-sm-4 col-form-label">Kode Pendaftaran</label></label>
+                    <div class="col-sm-8">
+                        <!-- add button cliport -->
+                        <input type="text" class="form-control" id="detailkode_pendaftaran" name="kode_pendaftaran"
+                            placeholder="Kode Pendaftaran" readonly>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="nisn" class="col-sm-4 col-form-label">Nisn</label></label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="detailnisn" name="nisn" placeholder="NISN" readonly>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="jenis_kelamin" class="col-sm-4 col-form-label">Jenis Kelamin</label></label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="detailjenis_kelamin" name="jenis_kelamin"
+                            placeholder="Jenis Kelamin" readonly>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="asal_sekolah" class="col-sm-4 col-form-label">Asal Sekolah</label></label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="detailasal_sekolah" name="asal_sekolah"
+                            placeholder="Asal Sekolah" readonly>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="no_tlp" class="col-sm-4 col-form-label">No telpon</label></label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="detailno_tlp" name="no_tlp" placeholder="No telpon"
+                            readonly>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="alamat" class="col-sm-4 col-form-label">alamat</label></label>
+                    <div class="col-sm-8">
+                        <textarea class="form-control" id="detailalamat" name="alamat" placeholder="Alamat"
+                            readonly></textarea>
+                    </div>
+                </div>
+                <div class="form-group row ">
+                    <label for="jalur_pendaftaran" class="col-sm-4 col-form-label">Jalur Pendaftaran</label></label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="jalur_pendaftaran" name="jalur_pendaftaran"
+                            placeholder="Jalur Pendaftaran" readonly>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                    Tutup
+                </button>
+            </div>
         </div>
     </div>
 </div>
@@ -172,6 +275,126 @@ function dataTablesAntrian() {
     });
 }
 dataTablesAntrian();
+
+
+function getSwall(status, message) {
+    swal({
+        title: message,
+        type: status == '200' ? 'success' : 'error',
+        showCancelButton: false,
+        showConfirmButton: true,
+        timer: 1500
+
+    })
+}
+
+const listFields = ['nama_siswa', 'nisn', 'jenis_kelamin', 'kode_pendaftaran', 'asal_sekolah', 'no_tlp', 'alamat',
+    'jalur_pendaftaran'
+];
+
+// tambah user
+$("#form_tambah_antrian").submit(function(e) {
+    e.preventDefault();
+    const formData = new FormData(this);
+    if (!this.checkValidity()) {
+        e.preventDefault();
+        $(this).addClass('form-control-success');
+    } else {
+        $("#btn_tambah_antiran").attr("disabled", "disabled");
+        $("#btn_tambah_antiran").html("Loading.....");
+        $.ajax({
+            url: '<?= base_url('Admin/Antrian/save') ?>',
+            method: 'post',
+            data: formData,
+            contentType: false,
+            cache: false,
+            processData: false,
+            dataType: 'json',
+            success: function(response) {
+                if (response.error) {
+                    $.each(response.data, function(key, value) {
+                        if (value != '') {
+                            $("#" + key).addClass('form-control-danger');
+                            $("#error" + key).addClass('has-danger');
+                            $("#error" + key).html(value);
+                        } else {
+                            $("#" + key).removeClass('form-control-danger');
+                            $("#" + key).addClass('form-control-success');
+                            $("#error" + key).html('');
+                            $("#error" + key).removeClass('has-danger');
+                        }
+                    });
+                } else {
+                    $("#form_tambah_antrian")[0].reset();
+                    $("#addAntrian").modal('hide');
+                    $('#tableAntrian').DataTable().ajax.reload();
+                    getSwall(response.status, response.data);
+                    users.forEach(function(item) {
+                        $("#" + item).removeClass('form-control-danger');
+                        $("#" + item).removeClass('form-control-success');
+                        $("#error" + item).html('');
+                        $("#error" + item).removeClass('has-danger');
+                    });
+                }
+                $("#btn_tambah_antiran").removeAttr("disabled");
+                $("#btn_tambah_antiran").html("Tambah");
+            }
+        });
+    }
+});
+
+// details user
+$(document).on('click', '.detailsAntrian', function() {
+    var id = $(this).attr('id');
+    $.ajax({
+        url: '<?= base_url('Admin/Antrian/edit') ?>',
+        method: 'post',
+        data: {
+            id: id
+        },
+        dataType: 'json',
+        success: function(response) {
+            $('#detailsAntrian').modal('show');
+            listFields.forEach(function(item) {
+                $("#detail" + item).val(response.data[item]);
+            });
+            $("#detailqr_code").attr('src', '<?= base_url('Assets/qr_code/') ?>' + response.data
+                .qr_code);
+        }
+    });
+});
+
+// hapus user
+$(document).on('click', '.delete_antrian', function() {
+    var id = $(this).attr('id');
+    swal({
+            title: "Apakah anda yakin?",
+            text: "Data yang dihapus tidak dapat dikembalikan!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonClass: "btn-danger",
+            confirmButtonText: "Ya, Hapus!",
+            confirmButtonClass: "btn btn-success margin-5",
+            cancelButtonText: "Batal",
+        })
+        .then((result) => {
+            if (result.value) {
+                $.ajax({
+                    url: '<?= base_url('Admin/Antrian/delete') ?>',
+                    method: 'post',
+                    data: {
+                        id: id
+                    },
+                    dataType: 'json',
+                    success: function(response) {
+                        $('#tableAntrian').DataTable().ajax.reload();
+                        getSwall(response.status, response.data);
+                    }
+                });
+            }
+        });
+});
+// copy clipboard
 </script>
 
 <?= $this->endSection('dataTables');?>s
