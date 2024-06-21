afaddfc5ed4bf2c9c4c07ab9effafeb7a01528cc
lammmzt <alam0098@stmik-wp.ac.id>
2024-06-21 16:14:35 +0700

    add fiture

--- app/Views/Admin/Antrian/laporan.php
+++ app/Views/Admin/Antrian/laporan.php
@@ -5,31 +5,64 @@
     <div class="col-md-12">
         <div class="card-box mb-30">
             <div class="pd-20">
-                <div class="row mb-4">
+                <div class="row mb-3">
                     <div class="col-sm-6">
                         <h4 class="text-blue h4">Data Antrian</h4>
                     </div>
                     <div class="col-sm-6 text-right">
-                        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#addAntrian" type="button">
+                        <!-- <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#addAntrian" type="button">
                             <i class="icon-copy fa fa-plus" aria-hidden="true"></i>
-                        </a>
+                        </a> -->
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group row">
-                            <label class="col-sm-12 col-md-3 col-form-label">tanggal_antrian</label>
-                            <div class="col-sm-12 col-md-9">
-                                <select class="custom-select col-12" id="tanggal_antrian">
-                                    <option value="">Pilih Tanggal</option>
-                                    <option value="2021-09-01">2021-09-01</option>
-                                    <option value="2021-09-02">2021-09-02</option>
-                                    <option value="2021-09-03">2021-09-03</option>
-                                </select>
+                <form id="form_laporan">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="col-sm-12 col-md-12 col-form-label">Tanggal Awal</label>
+                                <div class="col-sm-12 col-md-12">
+                                    <input class="form-control" type="date" id="tgl_awal">
+                                </div>
                             </div>
                         </div>
+
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="col-sm-12 col-md-12 col-form-label">Tanggal Akhir</label>
+                                <div class="col-sm-12 col-md-12">
+                                    <input class="form-control" type="date" id="tgl_akhir">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label class="col-sm-12 col-md-12 col-form-label">Status</label>
+                                <div class="col-sm-12 col-md-12">
+                                    <select class="custom-select col-12" id="status">
+                                        <option value="">Semua Antrean</option>
+                                        <option value="0">Tidak aktif</option>
+                                        <option value="2">Pemberkasan</option>
+                                        <option value="3">Selesai</option>
+                                        <option value="4">Bermasalah</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-1">
+                            <div class="form-group mt-4">
+                                <button class="btn btn-primary" id="btn-filter" type="button">
+                                    <i class="icon-copy fa fa-search" aria-hidden="true"></i>
+                                </button>
+                                <button class="btn btn-secondary" id="btn-reset" type="button">
+                                    <i class="icon-copy fa fa-refresh" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
+
                     </div>
-                </div>
+                </form>
                 <div class="pb-20 table-responsive">
                     <table class="table hover multiple-select-row nowrap" id="laporanAntrian">
                         <thead>
@@ -70,24 +103,11 @@
 // dataTables users
 
 
-$(document).ready(function() {
-    table = $('#laporanAntrian').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: {
-            url: '<?= base_url('Admin/Antrian/ajaxLaporan') ?>',
-            data: function(d) {
-                d.tanggal_antrian = $('#tanggal_antrian').val();
-            }
-        },
-    });
-
-    $('#tanggal_antrian').change(function(event) {
-        table.ajax.reload();
-        // alert('ok'); 
-    });
+$('#btn-filter').on('click', function() {
+    $('#btn-filter').html(
+        '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...'
+        );
 });
-
 
 function getSwall(status, message) {
     swal({
