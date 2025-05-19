969468620c9cacec858b784fd83fe27a3b8b05ec
lammmzt <alam0098@stmik-wp.ac.id>
2025-05-19 21:06:20 +0700

    add fiture

--- app/Views/landingPage/index.php
+++ app/Views/landingPage/index.php
@@ -1382,7 +1382,7 @@
         <div class="modal-dialog modal-dialog modal-xl modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalPDFLabel">Informasi PPDB 2024</h5>
+                    <h5 class="modal-title" id="modalPDFLabel">Informasi SPMB 2025</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
