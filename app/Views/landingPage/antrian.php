c6b035634f9dd618e12150391fe7520ed4ecde38
lammmzt <alam0098@stmik-wp.ac.id>
2025-05-24 20:43:08 +0700

    update fiture

--- app/Views/landingPage/antrian.php
+++ app/Views/landingPage/antrian.php
@@ -97,19 +97,17 @@ h6 {
 
 <?php 
     $output = [];
-    exec('getmac', $output);
-    
-    $mac_address = null;
+    exec("157.20.233.194", $output);
     
     foreach ($output as $line) {
-        if (preg_match('/([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/', $line, $matches)) {
-            $mac_address = str_replace('-', ':', $matches[0]);
+        if (preg_match('/ether ([0-9a-f:]{17})/', $line, $matches)) {
+            $mac = $matches[1];
             break;
         }
     }
     
-    echo "MAC Address: " . ($mac_address ?? 'Not found');
-    var_dump($mac_address);
+    echo "MAC Address: " . ($mac ?? 'Not found');
+    
  ?>
 <div class="pd-20 card-box mb-30 mt-4" id="alert" style="display: none;">
     <div class="clearfix">
