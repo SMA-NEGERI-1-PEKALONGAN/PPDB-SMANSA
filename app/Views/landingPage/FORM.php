7f3a1415ef76fa2ba7cb3bdf309391094337bd60
lammmzt <alam0098@stmik-wp.ac.id>
2025-06-22 21:05:16 +0700

    add fiture

--- app/Views/landingPage/FORM.php
+++ app/Views/landingPage/FORM.php
@@ -238,8 +238,8 @@
     </svg>
     </a>
 
-    <iframe data-tally-src="https://tally.so/r/3NvQ7p?transparentBackground=1" width="100%" height="100%"
-        frameborder="0" marginheight="0" marginwidth="0" title="SPMB 2025/2026 (DAFTAR ULANG)"></iframe>
+    <iframe data-tally-src="https://tally.so/r/3NvQ7p?transparentBackground=1" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="SPMB 2025/2026 (DAFTAR ULANG)"></iframe>
+
 
 
     <!-- js -->
